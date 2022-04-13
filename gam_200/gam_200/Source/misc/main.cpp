f08b4a16a892adbc43ba8e60e4c0a5a3855f45c6
Man Cong <w.mancong@digipen.edu>
2022-04-13 12:04:21 +0800

    More updates to Memory class
    1) If two bookmarks are beside each other, it will be combined into one instead
    2) Changes to UpdateIndex func
    - Initially did not set mFreed bookmarks starting from mIndex to it's default value
    - Right now mFreed bookmarks will be defaulted from the updated mIndex onwards
    3) mFreed and mAllocated bookmarks will now be sorted in ascending order

--- gam_200/gam_200/Source/misc/main.cpp
+++ gam_200/gam_200/Source/misc/main.cpp
@@ -3,7 +3,17 @@
 int main(void)
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	using ManCong::Memory::DynamicMemory;
 
-	ManCong::Memory::Reset();
+	int* ptr = DynamicMemory::New<int>();
+	int* ptr2 = DynamicMemory::New<int>();
+	int* ptr3 = DynamicMemory::New<int>();
+	DynamicMemory::Delete<int>(ptr);
+	DynamicMemory::Delete<int>(ptr3);
+	DynamicMemory::Delete<int>(ptr2);
+
+	//DynamicMemory::Delete<int>(ptr);
+	//DynamicMemory::Delete<int>(ptr3);
+
 	ManCong::Memory::FreeAll();
 }
