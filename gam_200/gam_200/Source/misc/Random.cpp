4f068117fdcc83d7916a720b2ec7e16be31ab803
Man Cong <w.mancong@digipen.edu>
2022-06-02 22:29:31 +0800

    Random
    Added a new function to determine random double floating point number.

--- gam_200/gam_200/Source/misc/Random.cpp
+++ gam_200/gam_200/Source/misc/Random.cpp
@@ -4,15 +4,27 @@ namespace ManCong
 {
 	namespace Random
 	{
+		namespace
+		{
+			std::mt19937 mt( std::random_device{}() );
+			std::uniform_int_distribution<s32> idis;
+			std::uniform_real_distribution<f32> fdis;
+			std::uniform_real_distribution<f64> ddis;
+		}
+
 		s32 Range(s32 min, s32 max)
 		{
-			std::random_device rd; std::mt19937 mt(rd()); std::uniform_int_distribution<s32> dis(min, max);
-			return dis(mt);
+			return idis(mt, decltype(idis)::param_type(min, max));
 		}
+
 		f32 Range(f32 min, f32 max)
 		{
-			std::random_device rd; std::mt19937 mt(rd()); std::uniform_real_distribution<f32> dist(min, max);
-			return dist(mt);
+			return fdis(mt, decltype(fdis)::param_type(min, max));
+		}
+
+		f64 Range(f64 min, f64 max)
+		{
+			return ddis(mt, decltype(ddis)::param_type(min, max));
 		}
 	}
 }
