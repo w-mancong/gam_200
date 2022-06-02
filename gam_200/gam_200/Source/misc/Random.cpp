d25d7d6bc7df2158a511f84341300fcef1ccb67d
Man Cong <w.mancong@digipen.edu>
2022-06-02 22:07:35 +0800

    Update Random.cpp
    name change from gen to mt

--- gam_200/gam_200/Source/misc/Random.cpp
+++ gam_200/gam_200/Source/misc/Random.cpp
@@ -6,13 +6,13 @@ namespace ManCong
 	{
 		s32 Range(s32 min, s32 max)
 		{
-			std::random_device rd; std::mt19937 gen(rd()); std::uniform_int_distribution<s32> dis(min, max);
-			return dis(gen);
+			std::random_device rd; std::mt19937 mt(rd()); std::uniform_int_distribution<s32> dis(min, max);
+			return dis(mt);
 		}
 		f32 Range(f32 min, f32 max)
 		{
-			std::random_device rd; std::mt19937 gen(rd()); std::uniform_real_distribution<f32> dist(min, max);
-			return dist(gen);
+			std::random_device rd; std::mt19937 mt(rd()); std::uniform_real_distribution<f32> dist(min, max);
+			return dist(mt);
 		}
 	}
 }
