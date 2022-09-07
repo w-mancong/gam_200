79489123b8354685a3a1b187760e4b49bceca40a
zafsire <extrazaf@gmail.com>
2022-09-07 11:39:26 +0800

    Removed fonts.h

--- Artless/ALEngine/Include/Graphics/Gizmo.h
+++ Artless/ALEngine/Include/Graphics/Gizmo.h
@@ -5,17 +5,14 @@
 
 namespace ALEngine
 {
-	namespace ECS
+	struct Gizmo
 	{
-		struct Gizmo
-		{
-			static f32 lineWidith;
+		static f32 lineWidith;
 
-			static void RenderLine(Math::Vector2 pt0, Math::Vector2 pt1);
-			static void DrawLineBox(Math::Vector2 pt0, Math::Vector2 pt1, Math::Vector2 pt2, Math::Vector2 pt3);
-			static void SetLineWidth(f32 width) { lineWidith = width; }
-		};
-	}
+		static void RenderLine(Math::Vector2 pt0, Math::Vector2 pt1);
+		static void DrawLineBox(Math::Vector2 pt0, Math::Vector2 pt1, Math::Vector2 pt2, Math::Vector2 pt3);
+		static void SetLineWidth(f32 width) { lineWidith = width; }
+	};
 }
 
 #endif
