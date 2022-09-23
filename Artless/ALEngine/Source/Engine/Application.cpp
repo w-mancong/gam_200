d34461fe0de186b2a42113d4c640192bb93575f6
VauserVonShepherd <vauservonshepherd@yahoo.com.sg>
2022-09-23 18:42:42 +0800

    Patch 0.9.2.4.2 (Add 4 walls into demo)
    -Prepared 4 walls for demo

--- Artless/ALEngine/Source/Engine/Application.cpp
+++ Artless/ALEngine/Source/Engine/Application.cpp
@@ -16,54 +16,67 @@ namespace ALEngine
 			void Exit(void);
 		};
 
-		Entity Noah, Hinata;
+		//Player
+		Entity player;
 
-		Entity LeftWall, RightWall;
+		//Boundaries
+		Entity BottomWall,LeftWall, RightWall, TopWall;
 
 		void Application::Init(void)
 		{
 			OpenGLWindow::InitGLFWWindow();
 			ECS::InitSystem();
 
-			//Obj 1
+			//Player
 			Transform transform{ Vector2(0, 150.0f), Vector2(50.f, 50.f), 0.0f };
-			Noah = CreateSprite(transform, Shape::Rectangle, RenderLayer::Background);
+			player = CreateSprite(transform, Shape::Rectangle, RenderLayer::Background);
 
-			Sprite& sprite2 = Coordinator::Instance()->GetComponent<Sprite>(Noah);
+			Sprite& sprite2 = Coordinator::Instance()->GetComponent<Sprite>(player);
 			sprite2.mode = RenderMode::Line;
 			sprite2.color = Color{ 1.0f, 0.0f, 0.0f, 1.0f };
 
-			Transform& trans_noah = Coordinator::Instance()->GetComponent<Transform>(Noah);
-			CreateCharacterController(Noah);
+			Transform& trans_noah = Coordinator::Instance()->GetComponent<Transform>(player);
+			CreateCharacterController(player);
+
 			//Character controller needs these
-			CreateCollider(Noah, ColliderType::Rectangle2D_AABB);
-			CreateRigidbody(Noah);
-			Coordinator::Instance()->GetComponent<Rigidbody2D>(Noah).isEnabled = true;
-			Coordinator::Instance()->GetComponent<Rigidbody2D>(Noah).drag.x = 0.25f;
-			Coordinator::Instance()->GetComponent<Rigidbody2D>(Noah).mass = 1.f;
-			Collider2D& collider_Noah = Coordinator::Instance()->GetComponent<Collider2D>(Noah);
+			CreateCollider(player, ColliderType::Rectangle2D_AABB);
+			CreateRigidbody(player);
+			Coordinator::Instance()->GetComponent<Rigidbody2D>(player).isEnabled = true;
+			Coordinator::Instance()->GetComponent<Rigidbody2D>(player).drag.x = 0.25f;
+			Coordinator::Instance()->GetComponent<Rigidbody2D>(player).mass = 1.f;
+			Collider2D& collider_Noah = Coordinator::Instance()->GetComponent<Collider2D>(player);
 			collider_Noah.scale[0] = 50.f, collider_Noah.scale[1] = 50.f;
 			collider_Noah.rotation = 0.f;
 			trans_noah.rotation = 0.f;
 			collider_Noah.isDebug = true;
 
-			Sprite& sprite = Coordinator::Instance()->GetComponent<Sprite>(Noah);
+			Sprite& sprite = Coordinator::Instance()->GetComponent<Sprite>(player);
 			sprite.mode = RenderMode::Line;
 			sprite.color = Color{ 1.0f, 1.0f, 1.0f, 1.0f };
 
-			//Obj 2
-			transform = { Vector2(0.0f, -200.0f), Vector2(5000.0f, 50.0f), 0.0f };
-			Hinata = CreateSprite(transform, Shape::Rectangle, RenderLayer::Background);
-
-			Sprite& sprite3 = Coordinator::Instance()->GetComponent<Sprite>(Hinata);
+			//Bottom Wall
+			transform = { Vector2(0.0f, -250.0f), Vector2(5000.0f, 50.0f), 0.0f };
+			BottomWall = CreateSprite(transform, Shape::Rectangle, RenderLayer::Background);
+			Sprite& sprite3 = Coordinator::Instance()->GetComponent<Sprite>(BottomWall);
 			sprite3.mode = RenderMode::Line;
 			sprite3.color = Color{ 1.0f, 1.0f, 1.0f, 1.0f };
-			Transform& trans_hinata = Coordinator::Instance()->GetComponent<Transform>(Hinata);
-			CreatePhysics2D(Hinata, ColliderType::Rectangle2D_AABB);
-			Collider2D& collider_hinata = Coordinator::Instance()->GetComponent<Collider2D>(Hinata);
-			collider_hinata.scale[0] = 5000.f, collider_hinata.scale[1] = 50.f;
+			CreatePhysics2D(BottomWall, ColliderType::Rectangle2D_AABB);
+			Collider2D& collider_bottom = Coordinator::Instance()->GetComponent<Collider2D>(BottomWall);
+			collider_bottom.scale[0] = 5000.f, collider_bottom.scale[1] = 50.f;
 
-			transform = { Vector2(-400.0f, 100.0f), Vector2(50.0f, 500.0f), 0.0f };
+			//Top Wall
+			transform = { Vector2(0.0f, 250.0f), Vector2(5000.0f, 50.0f), 0.0f };
+			TopWall = CreateSprite(transform, Shape::Rectangle, RenderLayer::Background);
+			Sprite& sprite6 = Coordinator::Instance()->GetComponent<Sprite>(TopWall);
+			sprite6.mode = RenderMode::Line;
+			sprite6.color = Color{ 1.0f, 1.0f, 1.0f, 1.0f };
+			CreatePhysics2D(TopWall, ColliderType::Rectangle2D_AABB);
+			Collider2D& collider_top = Coordinator::Instance()->GetComponent<Collider2D>(TopWall);
+			collider_top.scale[0] = 5000.0f, collider_top.scale[1] = 50.0f;
+
+
+			//Left Wall
+			transform = { Vector2(-500.0f, 0.0f), Vector2(50.0f, 500.0f), 0.0f };
 			LeftWall = CreateSprite(transform, Shape::Rectangle, RenderLayer::Background);
 			Sprite& sprite4 = Coordinator::Instance()->GetComponent<Sprite>(LeftWall);
 			sprite4.mode = RenderMode::Line;
@@ -72,14 +85,15 @@ namespace ALEngine
 			Collider2D& collider_left = Coordinator::Instance()->GetComponent<Collider2D>(LeftWall);
 			collider_left.scale[0] = 50.f, collider_left.scale[1] = 500.f;
 
-			transform = { Vector2(400, -22.0f), Vector2(200.0f, 300), 0.0f };
+			//Right
+			transform = { Vector2(500.0f, 0.0f), Vector2(200.0f, 425.0f), 0.0f };
 			RightWall = CreateSprite(transform, Shape::Rectangle, RenderLayer::Background);
 			Sprite& sprite5 = Coordinator::Instance()->GetComponent<Sprite>(RightWall);
 			sprite5.mode = RenderMode::Line;
 			sprite5.color = Color{ 1.0f, 1.0f, 1.0f, 1.0f };
 			CreatePhysics2D(RightWall, ColliderType::Rectangle2D_AABB);
 			Collider2D& collider_right = Coordinator::Instance()->GetComponent<Collider2D>(RightWall);
-			collider_right.scale[0] = 200.f, collider_right.scale[1] = 300.f;
+			collider_right.scale[0] = 200.f, collider_right.scale[1] = 425.0f;
 
 			// Initialize Time (Framerate Controller)
 			Time::Init();
