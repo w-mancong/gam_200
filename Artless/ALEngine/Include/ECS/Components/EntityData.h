1def5b802bb33288d954dc7e622e2fa2ee28afec
wowwlucas <lucas_nguyen@hotmail.com>
2022-09-28 12:49:41 +0800

    More comments - Lucas

--- Artless/ALEngine/Include/ECS/Components/EntityData.h
+++ Artless/ALEngine/Include/ECS/Components/EntityData.h
@@ -1,12 +1,24 @@
+/*!
+file:	EntityData.h
+author:	Lucas Nguyen
+email:	l.nguyen@digipen.edu
+brief:	This file contains the function declarations for the EntityData struct
+
+		All content © 2022 DigiPen Institute of Technology Singapore. All rights reserved.
+*//*__________________________________________________________________________________*/
 #ifndef ENTITY_DATA_H
 #define ENTITY_DATA_H
 
 namespace ALEngine::ECS::Component
 {
+	/*!*********************************************************************************
+		\brief
+			Data for the entity
+	***********************************************************************************/
 	struct EntityData
 	{
-		std::string tag{};
-		b8 active{ true };
+		std::string tag{};	// Entity Tag
+		b8 active{ true };	// Entity Active (For rendering)
 	};
 }
 
