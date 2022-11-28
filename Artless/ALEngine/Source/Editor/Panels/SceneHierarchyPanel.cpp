808474127f6d0bda4fbbf3398bc6d308f5a1c56e
wowwlucas <lucas_nguyen@hotmail.com>
2022-11-28 15:25:20 +0800

    Bug fix Scene Hierarchy Panel

--- Artless/ALEngine/Source/Editor/Panels/SceneHierarchyPanel.cpp
+++ Artless/ALEngine/Source/Editor/Panels/SceneHierarchyPanel.cpp
@@ -130,7 +130,7 @@ namespace ALEngine::Editor
 		}
 
 		// Delete selected entity
-		if (Input::KeyTriggered(KeyCode::Delete))
+		if (Input::KeyTriggered(KeyCode::Delete) && (ALEditor::Instance()->GetSelectedEntity() != ECS::MAX_ENTITIES))
 			remove = true;
 
 		// If there is an entity to remove
