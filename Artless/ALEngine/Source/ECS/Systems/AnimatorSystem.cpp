75791c64c03f849f0a5e5e00e0bb7c5ac18a5e6f
Man Cong <w.mancong@digipen.edu>
2023-03-05 10:57:18 +0800

    saving point

--- Artless/ALEngine/Source/ECS/Systems/AnimatorSystem.cpp
+++ Artless/ALEngine/Source/ECS/Systems/AnimatorSystem.cpp
@@ -178,14 +178,22 @@ namespace ALEngine::ECS
 
 	void ChangeAnimationClipName(ECS::Animator& animator, c8 const* newClipName, c8 const* oldClipName)
 	{
-		Animation animationClip = animator.animations[oldClipName];
+		auto node = animator.animations.extract(oldClipName);
+		node.key() = newClipName;
+		animator.animations.insert(std::move(node));
+		SaveAnimationClip(animator.animations[oldClipName], "Assets\\Animation\\" + std::string(newClipName) + ".anim");
+		remove( ("Assets\\Animation\\" + std::string(oldClipName) + ".anim").c_str() );
 
-		remove( ("Assets\\Animation\\" + std::string(animationClip.clipName) + ".anim").c_str() );
-		animator.animations.erase(oldClipName);
+		//SaveAnimationClip(animationClip, "Assets\\Animation\\" + std::string(animationClip.clipName) + ".anim");
 
-		strcpy_s(animationClip.clipName, sizeof(animationClip.clipName), newClipName);
-		SaveAnimationClip(animationClip, "Assets\\Animation\\" + std::string(animationClip.clipName) + ".anim");
-		animator.animations[newClipName] = animationClip;
+		//Animation animationClip = animator.animations[oldClipName];
+
+		//remove( ("Assets\\Animation\\" + std::string(animationClip.clipName) + ".anim").c_str() );
+		//animator.animations.erase(oldClipName);
+
+		//strcpy_s(animationClip.clipName, sizeof(animationClip.clipName), newClipName);
+		//
+		//animator.animations[newClipName] = animationClip;
 	}
 
 	void ChangeAnimationFilePath(Animation& animationClip, c8 const* filePath)
