0e271108d9bf3563bf0dcfa1e20a7350fd5a6ff6
Man Cong <w.mancong@digipen.edu>
2023-03-28 01:30:21 +0800

    Update Animation.h

--- Artless/ALEngine/Include/ECS/Components/Animation.h
+++ Artless/ALEngine/Include/ECS/Components/Animation.h
@@ -19,7 +19,7 @@ namespace ALEngine::ECS::Component
 		s32 width{ 128 };				// Value used to determine how wide	each sample from the spritesheet should be
 		s32 height{ 128 };				// Value used to determine how tall each sample from the spritesheet should be
 		u32 sample{ 60 };				// How fast should the animation be running (1/sample)
-		u32 totalSprites{ 0 };			// Total number of sample there is in the spritesheet
+		u32 totalSprites{ 1 };			// Total number of sample there is in the spritesheet
 		u32 frames[32]{ 1 };			// How long the frames will stay here for before moving to the next sprite
 		Guid id{ 0 };					// Id of animation to be used to retrieve texture handle from AssetManager
 		c8 clipName[128]{ '\0' };		// Name of the clip
