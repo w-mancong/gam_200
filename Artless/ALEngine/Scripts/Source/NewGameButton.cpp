c42150cc7e3a65c3ccf3bf2363056c1ffe60aee3
Man Cong <w.mancong@digipen.edu>
2023-03-29 02:05:46 +0800

    tutorial prompt done
    need to load the appropriate rooms when clicking yes/no

--- Artless/ALEngine/Scripts/Source/NewGameButton.cpp
+++ Artless/ALEngine/Scripts/Source/NewGameButton.cpp
@@ -12,11 +12,21 @@ brief:	This file contain function definition for new game button
 
 namespace ALEngine::Script
 {
+	void SetMap(u64 index);
+
 	namespace
 	{
 		using namespace ECS;
 		f32 constexpr ALPHA_VALUE{ 0.925f };
-		ECS::Entity scene_transition{ ECS::MAX_ENTITIES };
+		ECS::Entity scene_transition{ ECS::MAX_ENTITIES }, 
+			tutorial_prompt{ ECS::MAX_ENTITIES }, yes{ ECS::MAX_ENTITIES }, no{ ECS::MAX_ENTITIES };
+
+		void ChangeScene(u64 roomIndex)
+		{
+			std::shared_ptr<SceneChangeHelper> ptr = GetLogicComponent<SceneChangeHelper>(scene_transition);
+			ptr->NextScene();
+			SetMap(roomIndex);
+		}
 
 		void Darken(Entity en)
 		{
@@ -36,16 +46,37 @@ namespace ALEngine::Script
 				return;
 			Darken(en);
 			if (Input::KeyDown(KeyCode::MouseLeftButton))
-			{
-				std::shared_ptr<SceneChangeHelper> ptr = GetLogicComponent<SceneChangeHelper>(scene_transition);
-				ptr->NextScene();
-			}
+				SetActive(true, tutorial_prompt);
 		}
 
 		void WhenExit(Entity en)
 		{
 			if (ALPHA_VALUE > Coordinator::Instance()->GetComponent<Sprite>(en).color.a)
 				return;
+			Lighten(en);
+		}
+
+		void WhenYesHover(Entity en)
+		{
+			Darken(en);
+			if (Input::KeyDown(KeyCode::MouseLeftButton))
+				ChangeScene(0);
+		}
+
+		void WhenYesExit(Entity en)
+		{
+			Lighten(en);
+		}
+
+		void WhenNoHover(Entity en)
+		{
+			Darken(en);
+			if (Input::KeyDown(KeyCode::MouseLeftButton))
+				ChangeScene(0);
+		}
+
+		void WhenNoExit(Entity en)
+		{
 			Lighten(en);
 		}
 	}
@@ -67,7 +98,53 @@ namespace ALEngine::Script
 			ad.Play();
 		}
 
+		{
+			GetSceneGraph().FindImmediateChildren(en);
+			std::vector<s32> const& children = GetSceneGraph().GetChildren();
+
+			for (s32 child : children)
+			{
+				EntityData const& ed = Coordinator::Instance()->GetComponent<EntityData>(static_cast<Entity>(child));
+				if (ed.tag == "tutorial_prompt")
+					tutorial_prompt = static_cast<Entity>(child);
+			}
+		}
+
+		{
+			GetSceneGraph().FindImmediateChildren(tutorial_prompt);
+			std::vector<s32> const& children = GetSceneGraph().GetChildren();
+
+			for (s32 child : children)
+			{
+				EntityData const& ed = Coordinator::Instance()->GetComponent<EntityData>(static_cast<Entity>(child));
+				if (ed.tag == "yes")
+					yes = static_cast<Entity>(child);
+				else if (ed.tag == "no")
+					no  = static_cast<Entity>(child);
+			}
+
+			CreateEventTrigger(yes, true);
+			Subscribe(yes, Component::EVENT_TRIGGER_TYPE::ON_POINTER_STAY, WhenYesHover);
+			Subscribe(yes, Component::EVENT_TRIGGER_TYPE::ON_POINTER_EXIT, WhenYesExit);
+
+			CreateEventTrigger(no, true);
+			Subscribe(no, Component::EVENT_TRIGGER_TYPE::ON_POINTER_STAY, WhenNoHover);
+			Subscribe(no, Component::EVENT_TRIGGER_TYPE::ON_POINTER_EXIT, WhenNoExit);
+		}
+
 		Font::EnableTextRendering(true);
+	}
+
+	void NewGameButton::Update(ECS::Entity en)
+	{
+		if (Coordinator::Instance()->GetComponent<EntityData>(tutorial_prompt).active)
+		{
+			if (Input::KeyTriggered(KeyCode::Escape))
+			{
+				SetActive(false, tutorial_prompt);
+				Lighten(yes), Lighten(no);
+			}
+		}
 	}
 
 	void NewGameButton::Free(ECS::Entity en)
